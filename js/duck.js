6d1b495981c5e5e8364bbf99de61a4e73d9ad332
dansteele <danielsteele@hotmail.co.uk>
2015-02-15 10:42:22 +0000

    Duck adopts dead class on click.

--- js/duck.js
+++ js/duck.js
@@ -5,63 +5,48 @@ function Duck(game) {
   this.el = $("#duck-template").clone();
   this.el.removeAttr("id");
 
-  // Add a callback for when the Duck is clicked (shot!)
   var _this = this;
   $(this.el).click(function() {
     _this.die();
   });
 
-  // Display the Duck in the #game
   this.draw();
 }
 
-// A random height generator for use when placing a Duck.
 function randomHeight() {
   return 600 * Math.random();
 }
 
-// Some animation using a Timeout to make the Duck flap.
+
 Duck.prototype.flap = function() {
   $(this.el).toggleClass("flap");
-  // Oh Javascript...
   var _this = this;
 
-  // Do this again in 300 milliseconds
   this.flapTimer = setTimeout((function() {
   console.log("Flap")
     _this.flap();
   }), 300);
 }
 
-// TODO: Display the Duck on the screen.
 Duck.prototype.draw = function() {
-  // Make the duck appear somewhere random along the page and just off the screen
   el = this.el
   $(el).offset({top: randomHeight(), left: 30})  
   $(el).show()
-
-  // Append the element to the DOM, use the #game element
   $('#game').append(el)
-
-  // Start Flapping...
   this.flap()
-  // ... and Fly!
   $(el).animate({ top: randomHeight(), left: 1400 }, 3000, "linear", function() {
     this.remove()
   });
 }
 
-// TODO: I've been shot!
+
 Duck.prototype.die = function() {
-  // Add a .dead CSS class
-
-  // Stop flapping - clear the flapTimer
-
-  // Stop flying animations
-
-  // Notify the Game object and add 100 to the score
-
-  // Fall to the bottom of the screen
+  el = $(this.el)
+  el.addClass("dead")
+  clearTimeout(this.flapTimer)
+  el.stop()
+  this.game.addScore(100)
+  el.animate({top: 800, left: el.position.left}, 800, "linear")
 }
 
 // I made it to the other side!
